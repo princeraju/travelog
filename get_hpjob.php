f19c3a1d8da567bc02f695266cfcd312eafaf6d3
Prince Raju <princeraju7777@gmail.com>
2015-06-07 08:36:18 +0530

    Added hp config files

---
+++ get_hpjob.php
@@ -0,0 +1,55 @@
+ <?php
+
+
+function getSSLPage($url) {
+            $user_agent='Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 Firefox/8.0';
+
+        $options = array(
+
+            CURLOPT_CUSTOMREQUEST  =>"GET",        //set request type post or get
+            CURLOPT_POST           =>false,        //set to GET
+            CURLOPT_USERAGENT      => $user_agent, //set user agent
+            CURLOPT_COOKIEFILE     =>"cookie.txt", //set cookie file
+            CURLOPT_COOKIEJAR      =>"cookie.txt", //set cookie jar
+            CURLOPT_RETURNTRANSFER => true,     // return web page
+            CURLOPT_HEADER         => false,    // don't return headers
+            CURLOPT_FOLLOWLOCATION => false,     // follow redirects
+            CURLOPT_ENCODING       => "",       // handle all encodings
+            CURLOPT_AUTOREFERER    => true,     // set referer on redirect
+            CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect
+            CURLOPT_TIMEOUT        => 120,      // timeout on response
+            CURLOPT_MAXREDIRS      => 10,       // stop after 10 redirects
+            CURLOPT_SSL_VERIFYPEER =>false,
+            CURLOPT_SSL_VERIFYHOST =>false,
+        );
+
+        $ch      = curl_init( $url );
+        curl_setopt_array( $ch, $options );
+        $content = curl_exec( $ch );
+        $err     = curl_errno( $ch );
+        $errmsg  = curl_error( $ch );
+        $header  = curl_getinfo( $ch );
+        curl_close( $ch );
+
+        $header['errno']   = $err;
+        $header['errmsg']  = $errmsg;
+        $header['content'] = $content;
+        return $header;
+}
+
+function getjob($file)
+{
+           
+$url=$file;
+
+         $file="https://api.idolondemand.com/1/api/async/recognizespeech/v1?url=".$url."&apikey=".$hp_key;
+          $file=getSSLPage($file);
+print_r($file);
+$new=explode('"',$file["content"]);
+$new=$new[3];
+    return $new;
+    
+}
+
+?>
+
